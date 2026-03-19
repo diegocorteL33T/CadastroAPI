36d8e0c13e5e08a806a1df625414e954ad206989
Diego Corte <emptyyyxd@gmail.com>
2026-03-19 20:59:54 -0300

    fix: turns email a unique column

--- src/main/java/dev/java10x/cadastroapi/Users/Entity/UserEntity.java
+++ src/main/java/dev/java10x/cadastroapi/Users/Entity/UserEntity.java
@@ -21,7 +21,7 @@ public class UserEntity {
     @Column(nullable = false)
     private String name;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String email;
 
     @Column(nullable = false)
