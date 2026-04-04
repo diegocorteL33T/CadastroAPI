c1cf5130735aa4a724dc37baa08696e65e57db4c
diego <emptyyyxd@gmail.com>
2026-04-03 23:29:16 -0300

    chore: add UserDTO and UserMapper for user entity mapping

--- src/main/java/dev/java10x/usermanagementapi/Users/DTO/UserDTO.java
+++ src/main/java/dev/java10x/usermanagementapi/Users/DTO/UserDTO.java
@@ -1,4 +1,21 @@
 package dev.java10x.usermanagementapi.Users.DTO;
 
+import dev.java10x.usermanagementapi.Tasks.Entity.TaskEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class UserDTO {
+
+    private Long id;
+    private String name;
+    private String email;
+    private int age;
+    private String rank;
+    private TaskEntity task;
+
 }
--- src/main/java/dev/java10x/usermanagementapi/Users/Mapper/UserMapper.java
+++ src/main/java/dev/java10x/usermanagementapi/Users/Mapper/UserMapper.java
@@ -1,4 +1,37 @@
 package dev.java10x.usermanagementapi.Users.Mapper;
 
+
+import dev.java10x.usermanagementapi.Users.DTO.UserDTO;
+import dev.java10x.usermanagementapi.Users.Entity.UserEntity;
+import org.springframework.stereotype.Component;
+
+@Component
 public class UserMapper {
+
+    public UserEntity map(UserDTO userDTO) {
+
+        UserEntity userEntity = new UserEntity();
+        userEntity.setId(userDTO.getId());
+        userEntity.setName(userDTO.getName());
+        userEntity.setEmail(userDTO.getEmail());
+        userEntity.setAge(userDTO.getAge());
+        userEntity.setRank(userDTO.getRank());
+        userEntity.setTask(userDTO.getTask());
+
+        return userEntity;
+    }
+
+    public UserDTO map(UserEntity userEntity) {
+
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(userEntity.getId());
+        userDTO.setName(userEntity.getName());
+        userDTO.setEmail(userEntity.getEmail());
+        userDTO.setAge(userEntity.getAge());
+        userDTO.setRank(userEntity.getRank());
+        userDTO.setTask(userEntity.getTask());
+
+        return userDTO;
+    }
+
 }
