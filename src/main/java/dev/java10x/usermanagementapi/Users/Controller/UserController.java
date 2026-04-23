81f5162fd97da572b595484af7e0cdef2b7e7432
Diego Corte <emptyyyxd@gmail.com>
2026-04-23 14:31:22 -0300

    fix: update view name for user list page to follow naming conventions

--- src/main/java/dev/java10x/usermanagementapi/Users/Controller/UserController.java
+++ src/main/java/dev/java10x/usermanagementapi/Users/Controller/UserController.java
@@ -1,9 +1,11 @@
 package dev.java10x.usermanagementapi.Users.Controller;
 
 import dev.java10x.usermanagementapi.Users.DTO.UserDTO;
-import dev.java10x.usermanagementapi.Users.Entity.UserEntity;
 import dev.java10x.usermanagementapi.Users.Service.UserService;
-import org.apache.catalina.User;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,6 +24,11 @@ public class UserController {
     }
 
     @PostMapping("/register")
+    @Operation(summary = "Create a new user", description = "Registers a new user in the system with the provided details.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "User created successfully"),
+            @ApiResponse(responseCode = "400", description = "User creation error")
+    })
     public ResponseEntity<String> createUser(@RequestBody UserDTO user){
 
         /*
@@ -39,12 +46,22 @@ public class UserController {
     }
 
     @GetMapping("/list")
+    @Operation(summary = "Get all users", description = "Retrieves a list of all registered users in the system.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Users retrieved successfully"),
+            @ApiResponse(responseCode = "404", description = "No users found")
+    })
     public ResponseEntity<List<UserDTO>> showAllUsers() {
         List<UserDTO> users = service.showAllUsers();
         return ResponseEntity.ok(users);
     }
 
     @GetMapping("/list/{id}")
+    @Operation(summary = "Get user by ID", description = "Retrieves the details of a user by their ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User retrieved successfully"),
+            @ApiResponse(responseCode = "404", description = "User not found")
+    })
     public ResponseEntity<?> showUserByID(@PathVariable Long id){
         UserDTO user = service.showUserById(id);
         if(user != null){
@@ -57,7 +74,17 @@ public class UserController {
     }
 
     @PutMapping("/update/{id}")
-        public ResponseEntity<?> updateUser(@PathVariable Long id, @RequestBody UserDTO existingUser){
+    @Operation(summary = "Update user by ID", description = "Updates the details of an existing user by their ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200" , description = "User updated successfully"),
+            @ApiResponse(responseCode = "404", description = "User not found")
+    })
+        public ResponseEntity<?> updateUser(
+                @Parameter(description = "ID of the user to be updated", required = true)
+                @PathVariable Long id,
+                @Parameter(description = "Updated user details", required = true)
+                @RequestBody UserDTO existingUser){
+
         UserDTO user = service.updateUser(id, existingUser);
         if(user != null){
             return ResponseEntity.ok(user);
@@ -69,6 +96,11 @@ public class UserController {
     }
 
     @DeleteMapping("/delete/{id}")
+    @Operation(summary = "Delete user by ID", description = "Deletes an existing user by their ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User deleted successfully"),
+            @ApiResponse(responseCode = "404", description = "User not found")
+    })
     public ResponseEntity<String> deleteUserByID(@PathVariable Long id){
         UserDTO user = service.showUserById(id);
         if(user == null){
